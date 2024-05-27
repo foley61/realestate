9c7691b36be13890f05ab757dbed26561d65b4b1
foley61 <coder61616@gmail.com>
2024-05-27 15:52:23 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
-import HomePage from "./routes/homePage/HomePage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ListPage from "./routes/listPage/ListPage";
-import Layout from "./routes/layout/Layout";
-import SinglePage from "./routes/singlePage/singlePage";
-
-
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children:[
-        {
-          path:"/",
-          element:<HomePage/>
-        },
-        {
-          path:"/list",
-          element:<ListPage/>
-        },
-        {
-          path:"/:id",
-          element:<SinglePage/>
-        }
-  
-      ]
-    }
-  ]);
-
   return (
-
-    <RouterProvider router={router}/>
-  );
+    <div>Hello World</div>
+  )
 }
 
-export default App;
+export default App
