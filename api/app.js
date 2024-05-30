8b8328950f915812bc8a15af25887a51a3c570d6
foley61 <coder61616@gmail.com>
2024-05-30 14:42:40 +0300

    up

--- api/app.js
+++ api/app.js
@@ -1,13 +1,14 @@
 import express from "express"
 import postRoute from "./routes/post.route.js"
 import authRoute from "./routes/auth.route.js"
-
-
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 const app = express()
 app.use(express.json())
+dotenv.config()
 app.use("/api/posts",postRoute)
 app.use("/api/auth",authRoute)
-
+app.use(cookieParser())
 app.listen(8000, () => {
      console.log("running")
 })
