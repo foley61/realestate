98ffc328aea2c7743c192c2245c694814bef8fe4
foley61 <coder61616@gmail.com>
2024-06-03 15:27:27 +0300

    up

--- api/app.js
+++ api/app.js
@@ -3,7 +3,10 @@ import postRoute from "./routes/post.route.js"
 import authRoute from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
+import cors from "cors"
+
 const app = express()
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true}))
 app.use(express.json())
 dotenv.config()
 app.use("/api/posts",postRoute)
