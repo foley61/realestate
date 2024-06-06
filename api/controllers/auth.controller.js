2973f3f79b798acc3a85c1c4ea6f83f3f763c986
foley61 <coder61616@gmail.com>
2024-06-06 14:09:41 +0300

    up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,58 +1,84 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
-import jwt from "jsonwebtoken"
+
 export const register = async (req, res) => {
-    try {
-        const { username, email, password } = req.body
+  const { username, email, password } = req.body;
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    // HASH THE PASSWORD
 
-        const newUser = await prisma.user.create({
-            data: {
-                username, email, password: hashedPassword
-            }
-        })
-        console.log(newUser)
-        res.status(201).send(
-            newUser
-        )
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: "failed to create user!"
-        })
-    }
+    console.log(hashedPassword);
 
-}
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
+};
+
 export const login = async (req, res) => {
-    try {
-        const { username, password } = req.body
-        const user = await prisma.user.findUnique({
-            where: { username }
-        })
-        if (!user) return res.status(401).send({
-            message: "invalid credentials"
-        })
+  const { username, password } = req.body;
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+  try {
+    // CHECK IF THE USER EXISTS
 
-        if (!isPasswordValid) return res.status(401).json({ message: "Invalid credentials" })
-        const age = 1000 * 60 * 60 * 24 * 7;
-        const token = jwt.sign({
-            id: user.id,
-            isAdmin: true
-        }, process.env.JWT_SECRET_KEY, { expiresIn: age })
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            maxAge: age
-        }).status(200).json({ message: "login successful" })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ message: "Failed to login!" })
-    }
-}
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // CHECK IF THE PASSWORD IS CORRECT
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
+
 export const logout = (req, res) => {
-    res.clearCookie("token").status(200).json({ message: "Logout Successful" })
-}
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+};
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,9 +1,23 @@
-import { createContext,useState } from "react";
-export const AuthContext = createContext()
+import { createContext, useEffect, useState } from "react";
 
-export const AuthContextProvider = ({children}) => {
-    const [currentUser,setCurrentUser] = useState(
-        JSON.parse(localStorage.getItem("user")) || null
-    )
-    return <AuthContext.Provider value={{currentUser}}>{children}</AuthContext.Provider>
-}
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const updateUser = (data) => {
+    setCurrentUser(data);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser,updateUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
